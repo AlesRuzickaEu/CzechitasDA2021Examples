648fbb126874a411a5ec489e14e61716c2374332
Ales Ruzicka <a.ruzicka@live.com>
2021-04-17 11:28:21 +0200

    Auto commit 2021-04-17T11:28:21

--- index.js
+++ index.js
@@ -2,24 +2,31 @@
 
 const Post = (props) => {
   const element = document.createElement('div');
-  element.className = 'post';
+  element.classList.add('post');
   element.innerHTML = `<div class="post__text">
   ${props.text}
 </div>
-<button id="like-btn" class="like-btn">
+<button class="like-btn">
   <div class="like-icon"></div>
   <div>Olajkovat</div>
 </button>
   `;
+
+  const likeBtn = element.querySelector('.like-btn');
+  likeBtn.addEventListener('click', () => {
+    likeBtn.classList.toggle('like-btn--on');
+  });
+
   return element;
 };
 
 const appElement = document.querySelector('#app');
 
-appElement.appendChild(Post({ text: 'Muj status' }));
-appElement.appendChild(Post({ text: 'Muj status 2' }));
+const post1Props = { text: 'Muj status 1' };
+const post1 = Post(post1Props);
+appElement.appendChild(post1);
 
-// const likeBtn = document.querySelector('#like-btn');
-// likeBtn.addEventListener('click', () => {
-//   likeBtn.classList.toggle('like-btn--on');
-// });
+appElement.appendChild(Post({ text: 'Muj status 2' }));
+appElement.appendChild(Post({ text: 'Muj status 2' }));
+appElement.appendChild(Post({ text: 'Muj status 2' }));
+appElement.appendChild(Post({ text: 'Muj status 2' }));

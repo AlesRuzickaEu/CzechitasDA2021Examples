7c3cfa721e857f6d0c039e3cf4acb5bc08029590
Ales Ruzicka <a.ruzicka@live.com>
2021-04-17 10:12:35 +0200

    Auto commit 2021-04-17T10:12:34

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const appElement = document.querySelector('#app');
+
+const h1Element = document.createElement('h1');
+h1Element.textContent = 'Novy nadpis';
+h1Element.className = 'trida';
+h1Element.addEventListener('click', () => {
+  alert('kliknuto');
+});
+
+appElement.appendChild(h1Element);

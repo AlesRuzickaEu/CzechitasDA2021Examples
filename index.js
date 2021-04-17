db7523d77c9a6edd14fb46ef8715b920cd6315c4
Ales Ruzicka <a.ruzicka@live.com>
2021-04-17 11:09:11 +0200

    Auto commit 2021-04-17T11:09:11

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 'use strict';
 
+const Post = (props) => {
+  return `<div class="post">
+        <div class="post__text">
+          ${props.text}
+        </div>
+        <button id="like-btn" class="like-btn">
+          <div class="like-icon"></div>
+          <div>Olajkovat</div>
+        </button>
+      </div>`;
+};
+
+document.querySelector('#app').innerHTML = Post({ text: 'Muj status' });
+document.querySelector('#app').innerHTML += Post({ text: 'Muj status 2' });
+
 const likeBtn = document.querySelector('#like-btn');
 likeBtn.addEventListener('click', () => {
   likeBtn.classList.toggle('like-btn--on');

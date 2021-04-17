19378cd74e2a3962d0c2970e349e849eb3ae5141
Ales Ruzicka <a.ruzicka@live.com>
2021-04-17 11:15:17 +0200

    Auto commit 2021-04-17T11:15:17

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 'use strict';
 
 const Post = (props) => {
-  return `<div class="post">
-        <div class="post__text">
-          ${props.text}
-        </div>
-        <button id="like-btn" class="like-btn">
-          <div class="like-icon"></div>
-          <div>Olajkovat</div>
-        </button>
-      </div>`;
+  const element = document.createElement('div');
+  element.className = 'post';
+  element.innerHTML = `<div class="post__text">
+  ${props.text}
+</div>
+<button id="like-btn" class="like-btn">
+  <div class="like-icon"></div>
+  <div>Olajkovat</div>
+</button>
+  `;
+  return element;
 };
 
-document.querySelector('#app').innerHTML = Post({ text: 'Muj status' });
-document.querySelector('#app').innerHTML += Post({ text: 'Muj status 2' });
+const appElement = document.querySelector('#app');
 
-const likeBtn = document.querySelector('#like-btn');
-likeBtn.addEventListener('click', () => {
-  likeBtn.classList.toggle('like-btn--on');
-});
+appElement.appendChild(Post({ text: 'Muj status' }));
+appElement.appendChild(Post({ text: 'Muj status 2' }));
+
+// const likeBtn = document.querySelector('#like-btn');
+// likeBtn.addEventListener('click', () => {
+//   likeBtn.classList.toggle('like-btn--on');
+// });

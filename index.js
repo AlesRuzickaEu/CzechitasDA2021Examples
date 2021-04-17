2f82fd05f732135034ebc48cb3a4c2f6c9f91262
Ales Ruzicka <a.ruzicka@live.com>
2021-04-17 11:00:15 +0200

    Auto commit 2021-04-17T11:00:14

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 'use strict';
 
-const appElement = document.querySelector('#app');
-
-const h1Element = document.createElement('h1');
-h1Element.textContent = 'Novy nadpis';
-h1Element.className = 'trida';
-h1Element.addEventListener('click', () => {
-  alert('kliknuto');
+const likeBtn = document.querySelector('#like-btn');
+likeBtn.addEventListener('click', () => {
+  likeBtn.classList.toggle('like-btn--on');
 });
-
-appElement.appendChild(h1Element);
